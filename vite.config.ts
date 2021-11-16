6bb6b8db6ea106035f8ff0299a3ca0e6f6bfffa2
Mark Oroszi <mark@be-novative.com>
2021-11-16 13:19:34 +0100

    Add project root path alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // https://stackoverflow.com/a/66777245
+      "@": require("path").resolve(__dirname, "src"),
+    },
+  },
+});
